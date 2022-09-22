dad997ffe083fedbc1a1c3e1b1ecff07981c2fc1
erigra <erikgrande71@gmail.com>
2022-09-22 18:07:39 +0200

    kuler

--- starships_pygme.py
+++ starships_pygme.py
@@ -6,12 +6,20 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Eriks første Pythonspill")
 
+# Bruker definerte events
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
+
+
 # Skillestrek
 BORDER = pygame.Rect((WIDTH/2)-5, 0, 10, HEIGHT)
 
 #Globale variabler (konstanter egentlig)
 FPS = 60
 VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
 SPACESHIP_HEIGHT, SPACESHIP_WIDTH = 55,60
 
 # farger
@@ -36,47 +44,66 @@ def draw_window(red, yellow):
     WIN.blit(RED_SPACESHIP, (red.x,red.y))
     pygame.display.update()
 
-def yellow_handle_movement(keys_pressed, yellow): # The yellow spaceship (wasd)
+# The yellow spaceship  bevegelse (wasd)
+def yellow_handle_movement(keys_pressed, yellow): 
         if keys_pressed[pygame.K_a] and yellow.x - VEL > 0 :
-            yellow.x-=VEL
+            yellow.x -= VEL
         if keys_pressed[pygame.K_d] and yellow.x + VEL + SPACESHIP_WIDTH < BORDER.x:
-            yellow.x+=VEL     
+            yellow.x += VEL     
         if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:
-            yellow.y-=VEL
+            yellow.y -= VEL
         if keys_pressed[pygame.K_s] and yellow.y + VEL + SPACESHIP_HEIGHT < HEIGHT -10:
-            yellow.y+=VEL
+            yellow.y += VEL
 
-def red_handle_movement(keys_pressed, red): # The red spaceship  (piltaster)
-    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x +10 :
-        red.x-=VEL
+# The red spaceship bevegelse (piltaster)
+def red_handle_movement(keys_pressed, red): 
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + 10 :
+        red.x -= VEL
     if keys_pressed[pygame.K_RIGHT] and red.x + VEL + SPACESHIP_WIDTH < WIDTH:
-        red.x+=VEL     
+        red.x += VEL     
     if keys_pressed[pygame.K_UP] and red.y - VEL > 0:
-        red.y-=VEL
+        red.y -= VEL
     if keys_pressed[pygame.K_DOWN] and red.y + VEL + SPACESHIP_HEIGHT < HEIGHT -10:
-        red.y+=VEL
+        red.y += VEL
 
-
+# Kuler bevegelse og hit sjekk
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += 1
+        if yellow.colliderect(bullet)
 
 
 # Hovedprogram starter her :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 def main():
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    
+    red_bullets=[]
+    yellow_bullets=[]
+
     clock = pygame.time.Clock()
+   
     run = True 
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                 run = False
-        
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet=pygame.Rect(yellow.x+yellow.width, yellow.y + yellow.height/2 + 2, 10, 5)
+                    yellow_bullets.append(bullet)
+
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet=pygame.Rect(red.x, red.y + red.height/2 + 2, 10, 5)
+                    red_bullets.append(bullet)
+
         keys_pressed = pygame.key.get_pressed()
-        
         yellow_handle_movement(keys_pressed, yellow)
-        
         red_handle_movement(keys_pressed, red)
         
+        handle_bullets(yellow_bullets, red_bullets, yellow, red)
 
         draw_window(red, yellow)
 
